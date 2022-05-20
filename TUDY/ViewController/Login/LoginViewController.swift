19e7f0971d3a269f1381def81da48ba2ef3c87eb
neuli <asdfz888@naver.com>
2022-05-20 09:31:33 +0900

    [Feat] 애플로그인 구현 - #2
    코드 구현, 추가 설정 필요

--- TUDY/ViewController/Login/LoginViewController.swift
+++ TUDY/ViewController/Login/LoginViewController.swift
@@ -5,6 +5,8 @@
 //  Created by Hojin Jang on 2022/05/18.
 //
 
+import AuthenticationServices
+import CryptoKit
 import UIKit
 
 import FirebaseAuth
@@ -23,15 +25,15 @@ class LoginViewController: UIViewController {
         return button
     }()
     
+    private let appleLoginButton = ASAuthorizationAppleIDButton(type: .signIn, style: .white)
+    private var currentNonce: String?
+    
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
     }
-}
-
-extension LoginViewController {
     
     // MARK: - Helper Functions
     
@@ -45,7 +47,118 @@ extension LoginViewController {
             make.centerX.equalTo(view.snp.centerX)
             make.centerY.equalTo(view.snp.centerY)
         }
+        
+        view.addSubview(appleLoginButton)
+        appleLoginButton.addTarget(self, action: #selector(appleLoginButtonTapped(_:)), for: .touchUpInside)
+        appleLoginButton.snp.makeConstraints { make in
+            make.centerX.equalTo(view.snp.centerX)
+            make.top.equalTo(kakaoLoginButton.snp.bottom).offset(20)
+        }
     }
+}
+
+// MARK: - Apple Login
+
+extension LoginViewController: ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
+    
+    @objc private func appleLoginButtonTapped(_ sender: UIButton) {
+            
+        let nonce = randomNonceString()
+        currentNonce = nonce
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        request.nonce = sha256(nonce)
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+    
+    // Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
+    private func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        let charset: Array<Character> =
+        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
+        
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+                }
+                return random
+            }
+            
+            randoms.forEach { random in
+                if remainingLength == 0 {
+                    return
+                }
+                
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+        
+        return result
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            guard let nonce = currentNonce else {
+                fatalError("Invalid state: A login callback was received, but no login request was sent.")
+            }
+            guard let appleIDToken = appleIDCredential.identityToken else {
+                print("Unable to fetch identity token")
+                return
+            }
+            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+                return
+            }
+            
+            let credential = OAuthProvider.credential(withProviderID: "apple.com", idToken: idTokenString, rawNonce: nonce)
+            
+            Auth.auth().signIn(with: credential) { authResult, error in
+                if let error = error {
+                    print ("Error Apple sign in: %@", error)
+                    return
+                }
+                // User is signed in to Firebase with Apple.
+                // ...
+                ///Main 화면으로 보내기
+                //                let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+                //                let mainViewController = storyboard.instantiateViewController(identifier: "MainViewController")
+                //                mainViewController.modalPresentationStyle = .fullScreen
+                //                self.navigationController?.show(mainViewController, sender: nil)
+            }
+        }
+    }
+    
+    private func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+            return String(format: "%02x", $0)
+        }.joined()
+        
+        return hashString
+    }
+    
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+}
+
+// MARK: - Kakao Login
+
+extension LoginViewController {
     
     @objc private func kakaoLoginButtonTapped(_ sender: UIButton) {
         
@@ -124,9 +237,8 @@ extension LoginViewController {
             }
         }
     }
-    
 
-    // MARK: - 임시 Functions
+    // MARK: 임시 Functions
     
     // 토큰 정보 보기
     private func kakaoAccessTokenInfo() {
