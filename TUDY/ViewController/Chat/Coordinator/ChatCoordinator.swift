d2e33deaae8862b6bb72238a9d9a2dcbcd3e0132
neuli <asdfz888@naver.com>
2022-05-23 21:20:37 +0900

    [Feat] TabBar Coordinator 생성 (임시)
    탭 - 홈화면, 채팅화면 코디네이터 생성

---
+++ TUDY/ViewController/Chat/Coordinator/ChatCoordinator.swift
@@ -0,0 +1,35 @@
+//
+//  ChatCoordinator.swift
+//  TUDY
+//
+//  Created by neuli on 2022/05/23.
+//
+
+import UIKit
+
+final class ChatCoordinator: ChatCoordinatorProtocol {
+    
+    var chatViewController: ChatViewController
+    
+    weak var finishDelegate: CoordinatorFinishDelegate?
+    var navigationController: UINavigationController
+    var childCoordinators: [Coordinator] = []
+    var type: CoordinatorType = .chat
+    
+    init(_ navigationController: UINavigationController) {
+        self.navigationController = navigationController
+        self.chatViewController = ChatViewController()
+    }
+    
+    func start() {
+        self.navigationController.pushViewController(self.chatViewController, animated: true)
+    }
+}
+
+extension ChatCoordinator: CoordinatorFinishDelegate {
+    
+    func coordinatorDidFinish(childCoordinator: Coordinator) {
+        self.childCoordinators = self.childCoordinators.filter { $0.type != childCoordinator.type }
+        childCoordinator.navigationController.popToRootViewController(animated: true)
+    }
+}
---
+++ TUDY/ViewController/Home/Coordinator/HomeCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  HomeCoordinator.swift
+//  TUDY
+//
+//  Created by neuli on 2022/05/23.
+//
+
+import Foundation
