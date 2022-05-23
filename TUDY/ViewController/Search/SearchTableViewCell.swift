eda635fb21843d6da4b63780e19899ceef877eb8
change-challenge <dog941128@hotmail.com>
2022-05-23 19:05:58 +0900

    [Feat] 검색창 뷰 레이아웃 잡기 - issue: #1 - 검색창 VC 레이아웃 만들기 - 최근 검색어 보기/추가 - 전체 삭제 버튼 - 각 테이블 뷰에서 cell 삭제

--- TUDY/ViewController/Search/SearchTableViewCell.swift
+++ TUDY/ViewController/Search/SearchTableViewCell.swift
@@ -8,18 +8,45 @@
 import UIKit
 
 class SearchTableViewCell: UITableViewCell {
-
-    let currentsearchText = UILabel()
-    let cancelButton = UIButton()
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    // MARK: - Properties
+    lazy var currentsearchText: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = .black
+        return label
+    }()
+    
+    var cancelButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("x", for: .normal)
+        button.setTitleColor(.lightGray, for: .normal)
+        return button
+    }()
+
+    // MARK: - Lift Cycle
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setUp()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Methods
+    private func setUp() {
+        self.addSubview(currentsearchText)
+        self.contentView.addSubview(cancelButton)
         
+        currentsearchText.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(10)
+            make.centerY.equalToSuperview()
+        }
+        
+        cancelButton.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().offset(-10)
+            make.centerY.equalToSuperview()
+        }
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-    }
-    
-
 }
--- TUDY/ViewController/Search/SearchViewController.swift
+++ TUDY/ViewController/Search/SearchViewController.swift
@@ -6,18 +6,21 @@
 //
 
 import UIKit
+import SwiftUI
 
 
 class SearchViewController: UIViewController {
 
+    // MARK: - Properties
     let cellId: String = "Cell"
-    let list = ["이커머스", "ios개발", "취준"]
+    var list = ["이커머스", "ios개발", "취준"]
 
     lazy var searchbar: UISearchBar = {
        let searchbar = UISearchBar()
         searchbar.placeholder = "검색어를 입력해주세요."
         searchbar.searchTextField.layer.cornerRadius = 15
         searchbar.searchTextField.layer.masksToBounds = true
+        searchbar.setImage(UIImage(named: "icCancel"), for: .clear, state: .normal)
         return searchbar
     }()
 
@@ -31,18 +34,18 @@ class SearchViewController: UIViewController {
         return label
     }()
     
-    lazy var alldeleteLabel: UILabel = {
-        let label = UILabel()
-        label.text = "전체 삭제"
-        label.textColor = .lightGray
-        label.font = UIFont.systemFont(ofSize: 13)
-        return label
+    lazy var alldeleteLabel: UIButton = {
+        let button = UIButton()
+        button.setTitle("전체 삭제", for: .normal)
+        button.setTitleColor(.lightGray, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 13)
+        button.addTarget(self, action: #selector(deleteAllSearch(_:)), for: .touchUpInside)
+        return button
     }()
     
     
     lazy var bodytitlestackView: UIStackView = {
         let stackV = UIStackView(arrangedSubviews: [titleLabel, alldeleteLabel])
-        stackV.translatesAutoresizingMaskIntoConstraints = false
         stackV.axis = .horizontal
         stackV.alignment = .center
         stackV.distribution = .equalSpacing
@@ -51,38 +54,44 @@ class SearchViewController: UIViewController {
     
     lazy var searchTableView: UITableView = {
         let cv = UITableView()
-        cv.backgroundColor = .purple
+        cv.register(SearchTableViewCell.self, forCellReuseIdentifier: cellId)
         return cv
     }()
 
-
-
+    // MARK: - Lift Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         searchTableView.delegate = self
         searchTableView.dataSource = self
+        searchbar.delegate = self
         setNav()
         setUp()
+        self.searchbar.becomeFirstResponder()
     }
     
+    // MARK: - Methods
     func setUp() {
         view.addSubview(bodyview)
         bodyview.addSubview(bodytitlestackView)
+        bodyview.addSubview(searchTableView)
         
-        
-        bodyview.backgroundColor = .red
         bodyview.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(10)
             make.leading.bottom.trailing.equalToSuperview()
         }
         
-        bodytitlestackView.backgroundColor = .yellow
         bodytitlestackView.snp.makeConstraints { make in
             make.top.equalToSuperview()
             make.centerX.equalToSuperview()
-            make.bottom.equalToSuperview().multipliedBy(0.1)
+            make.height.equalToSuperview().multipliedBy(0.08)
             make.width.equalToSuperview().multipliedBy(0.92)
+        }
+        
+        searchTableView.snp.makeConstraints { make in
+            make.top.equalTo(bodytitlestackView.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalToSuperview()
         }
     }
 
@@ -91,18 +100,46 @@ class SearchViewController: UIViewController {
         self.navigationController?.navigationBar.tintColor = .black
         self.navigationController?.navigationBar.topItem?.title = ""
     }
- 
+    
+    @objc func deleteSearch(_ sender:UIButton) {
+        let indexRow = sender.tag
+        list.remove(at: indexRow)
+        searchTableView.deleteRows(at: [IndexPath(row: indexRow, section: 0)], with: .left)
+        searchTableView.reloadData()
+        if (list == []) {
+            bodyview.removeFromSuperview()
+        }
+    }
+    
+    @objc func deleteAllSearch(_: UIButton) {
+        list.removeAll()
+        searchTableView.reloadData()
+        bodyview.removeFromSuperview()
+    }
 }
 
+    // MARK: - extensions
 extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return list.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! SearchTableViewCell
-        cell.
+        cell.currentsearchText.text = list[indexPath.row]
+        cell.cancelButton.tag = indexPath.row
+        cell.cancelButton.addTarget(self, action: #selector(deleteSearch(_:)), for: .touchUpInside)
+        return cell
     }
-    
-    
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let text = searchbar.text else {
+            return
+        }
+        list.append(text)
+        searchTableView.reloadData()
+    }
 }
