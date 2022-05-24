30e7a305c6fe5b4ba9af12e5c0947582a1b82d6d
oyat2bit <oyat2bit@gmail.com>
2022-05-24 14:59:02 +0900

    [Fixed] Conflict Error

--- TUDY/ViewController/Search/SearchViewController.swift
+++ TUDY/ViewController/Search/SearchViewController.swift
@@ -6,108 +6,144 @@
 //
 
 import UIKit
+import SwiftUI
+
 
 class SearchViewController: UIViewController {
 
-    let cellId = "Cell"
-    let progTheme = ["웹개발", "iOS개발", "안드로이드개발", "서버개발", "AI개발", "게임개발"]
-    let designTheme = ["UI/UX디자인", "게임디자인"]
+    // MARK: - Properties
+    let cellId: String = "Cell"
+    var list = ["이커머스", "ios개발", "취준"]
+
+    lazy var searchbar: UISearchBar = {
+       let searchbar = UISearchBar()
+        searchbar.placeholder = "검색어를 입력해주세요."
+        searchbar.searchTextField.layer.cornerRadius = 15
+        searchbar.searchTextField.layer.masksToBounds = true
+        searchbar.setImage(UIImage(named: "icCancel"), for: .clear, state: .normal)
+        return searchbar
+    }()
+
+    let bodyview = UIView()
     
     lazy var titleLabel: UILabel = {
         let label = UILabel()
-        label.text = "직무를 선택해주세요."
+        label.text = "최근 검색어"
         label.textColor = .black
-        label.font = label.font.withSize(20)
+        label.font = UIFont.boldSystemFont(ofSize: 17)
         return label
     }()
     
-    lazy var computerLabel: UILabel = {
-        let label = UILabel()
-        label.text = "개발자"
-        label.textColor = .lightGray
-        label.font = label.font.withSize(15)
-        return label
+    lazy var alldeleteLabel: UIButton = {
+        let button = UIButton()
+        button.setTitle("전체 삭제", for: .normal)
+        button.setTitleColor(.lightGray, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 13)
+        button.addTarget(self, action: #selector(deleteAllSearch(_:)), for: .touchUpInside)
+        return button
     }()
     
-    lazy var designLabel: UILabel = {
-        let label = UILabel()
-        label.text = "디자이너"
-        label.textColor = .lightGray
-        label.font = label.font.withSize(15)
-        return label
+    
+    lazy var bodytitlestackView: UIStackView = {
+        let stackV = UIStackView(arrangedSubviews: [titleLabel, alldeleteLabel])
+        stackV.axis = .horizontal
+        stackV.alignment = .center
+        stackV.distribution = .equalSpacing
+        return stackV
     }()
     
-    lazy var themeCollectionView: UICollectionView = {
-        let flowlayout = CollectionViewLeftAlignFlowLayout()
-        let cv = UICollectionView(frame: .zero, collectionViewLayout: flowlayout)
-        cv.backgroundColor = .yellow
-        cv.register(SearchCollectionViewCell.self, forCellWithReuseIdentifier: cellId)
-        if let flowLayout = cv.collectionViewLayout as? UICollectionViewFlowLayout {
-                    flowLayout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
-                  }
+    lazy var searchTableView: UITableView = {
+        let cv = UITableView()
+        cv.register(SearchTableViewCell.self, forCellReuseIdentifier: cellId)
         return cv
     }()
-    
-    
+
+    // MARK: - Lift Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        self.navigationItem.title = "TUDY"
+        searchTableView.delegate = self
+        searchTableView.dataSource = self
+        searchbar.delegate = self
+        setNav()
         setUp()
-        themeCollectionView.delegate = self
-        themeCollectionView.dataSource = self
+        self.searchbar.becomeFirstResponder()
     }
     
+    // MARK: - Methods
     func setUp() {
-        view.addSubview(titleLabel)
-        view.addSubview(computerLabel)
-        view.addSubview(designLabel)
-        view.addSubview(themeCollectionView)
+        view.addSubview(bodyview)
+        bodyview.addSubview(bodytitlestackView)
+        bodyview.addSubview(searchTableView)
         
-        titleLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(100)
-            make.leading.equalToSuperview().offset(20)
-            make.width.equalToSuperview().multipliedBy(0.5)
-            make.height.equalToSuperview().multipliedBy(0.05)
+        bodyview.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(10)
+            make.leading.bottom.trailing.equalToSuperview()
         }
         
-        themeCollectionView.snp.makeConstraints { make in
-            make.top.equalTo(titleLabel.snp.bottom).offset(10)
+        bodytitlestackView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
             make.centerX.equalToSuperview()
-            make.width.equalToSuperview().multipliedBy(0.9)
-            make.height.equalToSuperview().multipliedBy(0.5)
+            make.height.equalToSuperview().multipliedBy(0.08)
+            make.width.equalToSuperview().multipliedBy(0.92)
         }
         
-        computerLabel.snp.makeConstraints { make in
-            make.top.equalTo(themeCollectionView.snp.bottom)
-            make.leading.equalToSuperview().offset(20)
-            make.width.equalToSuperview().multipliedBy(0.5)
-            make.height.equalTo(titleLabel)
+        searchTableView.snp.makeConstraints { make in
+            make.top.equalTo(bodytitlestackView.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalToSuperview()
         }
+    }
+
+    func setNav() {
+        self.navigationItem.titleView = searchbar
+        self.navigationController?.navigationBar.tintColor = .black
+        self.navigationController?.navigationBar.topItem?.title = ""
+    }
     
-        designLabel.snp.makeConstraints { make in
-            make.top.equalTo(computerLabel.snp.bottom).offset(100)
-            make.leading.equalToSuperview().offset(20)
-            make.width.equalToSuperview().multipliedBy(0.5)
-            make.height.equalTo(titleLabel)
+    @objc func deleteSearch(_ sender:UIButton) {
+        let indexRow = sender.tag
+        list.remove(at: indexRow)
+        searchTableView.deleteRows(at: [IndexPath(row: indexRow, section: 0)], with: .left)
+        searchTableView.reloadData()
+        if (list == []) {
+            bodyview.removeFromSuperview()
         }
+    }
+    
+    @objc func deleteAllSearch(_: UIButton) {
+        list.removeAll()
+        searchTableView.reloadData()
+        bodyview.removeFromSuperview()
     }
 }
 
-
-extension SearchViewController: UICollectionViewDelegate {
+    // MARK: - extensions
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     
-}
-
-extension SearchViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return progTheme.count
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! SearchCollectionViewCell
-        cell.label.text = progTheme[indexPath.item]
-        cell.backgroundColor = .white
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! SearchTableViewCell
+        cell.currentsearchText.text = list[indexPath.row]
+        cell.cancelButton.tag = indexPath.row
+        cell.cancelButton.addTarget(self, action: #selector(deleteSearch(_:)), for: .touchUpInside)
         return cell
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let text = searchbar.text else {
+            return
+        }
+        if (list.isEmpty) {
+            setUp()
+        }
+        list.append(text)
+        searchbar.text = ""
+        searchTableView.reloadData()
     }
 }
