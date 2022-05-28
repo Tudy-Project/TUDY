c40394ba96a2dd7b7c8a2e496f0613153b68e9e7
neuli <asdfz888@naver.com>
2022-05-28 20:56:47 +0900

    [Feat] Font, Color 추가
    extrabold 16, color set 추가

--- TUDY/Util/Extension/UIColor+Custom.swift
+++ TUDY/Util/Extension/UIColor+Custom.swift
@@ -9,7 +9,19 @@ import UIKit
 
 extension UIColor {
     
-    static var mainColor: UIColor {
-        UIColor(red: 0, green: 0, blue: 0, alpha: 1.0)
-    }
+    static var DarkGray1: UIColor { UIColor(named: "Dark-gray1") ?? UIColor.black }
+    static var DarkGray2: UIColor { UIColor(named: "Dark-gray2") ?? UIColor.black }
+    static var DarkGray3: UIColor { UIColor(named: "Dark-gray3") ?? UIColor.black }
+    static var DarkGray4: UIColor { UIColor(named: "Dark-gray4") ?? UIColor.darkGray }
+    static var DarkGray5: UIColor { UIColor(named: "Dark-gray5") ?? UIColor.darkGray }
+    static var DarkGray6: UIColor { UIColor(named: "Dark-gray6") ?? UIColor.darkGray }
+    
+    static var LightGray1: UIColor { UIColor(named: "Light-gray1") ?? UIColor.gray }
+    static var LightGray2: UIColor { UIColor(named: "Light-gray2") ?? UIColor.gray }
+    static var LightGray3: UIColor { UIColor(named: "Light-gray3") ?? UIColor.gray }
+    static var LightGray4: UIColor { UIColor(named: "Light-gray4") ?? UIColor.gray }
+    static var LightGray5: UIColor { UIColor(named: "Light-gray5") ?? UIColor.gray }
+    static var LightGray6: UIColor { UIColor(named: "Light-gray6") ?? UIColor.gray }
+    
+    static var White: UIColor { UIColor(named: "White") ?? UIColor.white }
 }
