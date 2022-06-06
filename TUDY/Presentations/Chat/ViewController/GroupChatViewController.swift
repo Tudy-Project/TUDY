812917367163d455f94109b80792aab7ec7d038a
neuli <asdfz888@naver.com>
2022-06-06 15:01:11 +0900

    [Feat] 개인, 그룹 챗 ViewController 생성 - #42

---
+++ TUDY/Presentations/Chat/ViewController/GroupChatViewController.swift
@@ -0,0 +1,23 @@
+//
+//  GroupChatViewController.swift
+//  TUDY
+//
+//  Created by neuli on 2022/06/06.
+//
+
+import UIKit
+
+class GroupChatViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension GroupChatViewController {
+    
+    // MARK: - Methods
+}
---
+++ TUDY/Presentations/Chat/ViewController/PersonalChatViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PersonalViewController.swift
+//  TUDY
+//
+//  Created by neuli on 2022/06/06.
+//
+
+import UIKit
+
+class PersonalChatViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension PersonalChatViewController {
+    
+    // MARK: - Methods
+}
