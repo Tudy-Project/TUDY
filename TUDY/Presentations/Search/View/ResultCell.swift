8d8cee41f09565fc189ac55f35b9e37cd766fa14
change-challenge <dog941128@hotmail.com>
2022-06-09 02:19:05 +0900

    [Feat] New 검색창 최근 검색어 레이아웃-(#55) - issue: #55 - 최근 검색창을 collectionview로 구성 - 하나씩 선택해서 삭제할 필요가 있는 지 의문이 들어 리스트만 보여주기로 만들 었습니다.

---
+++ TUDY/Presentations/Search/View/ResultCell.swift
@@ -0,0 +1,57 @@
+//
+//  SearchResultCollectionViewCell.swift
+//  TUDY
+//
+//  Created by Hojin Jang on 2022/06/08.
+//
+
+import UIKit
+
+class ResultCell: UICollectionViewCell {
+
+    // MARK: - Properties
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.backgroundColor = UIColor.DarkGray5
+        label.font = UIFont.systemFont(ofSize: 12)
+        return label
+    }()
+    
+    // MARK: - Lift Cycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureUI()
+    }
+}
+
+    // MARK: - Methods
+extension ResultCell {
+    static func fittingSize(availableHeight: CGFloat, name: String?) -> CGSize {
+        let cell = ResultCell()
+        cell.configureLabel(name: name)
+        
+        let targetSize = CGSize(width: UIView.layoutFittingCompressedSize.width + 10, height: availableHeight)
+        return cell.contentView.systemLayoutSizeFitting(targetSize, withHorizontalFittingPriority: .fittingSizeLevel, verticalFittingPriority: .required)
+    }
+    
+    private func configureUI() {
+        backgroundColor = UIColor.DarkGray1
+        titleLabel.textAlignment = .center
+        titleLabel.textColor = .white
+        
+        contentView.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview().inset(15)
+        }
+    }
+    
+    func configureLabel(name: String?) {
+        titleLabel.text = name
+    }
+}
--- TUDY/Presentations/Search/ViewController/SearchViewController.swift
+++ TUDY/Presentations/Search/ViewController/SearchViewController.swift
@@ -13,13 +13,17 @@ class SearchViewController: UIViewController {
 
     // MARK: - Properties
     let cellId: String = "Cell"
-    var list = ["이커머스", "ios개발", "취준"]
+    var list = ["이커머스", "ios개발", "취준", "안녕하세요!!!!", "이것은 몇 글자일까?!?!?!"]
 
     lazy var searchbar: UISearchBar = {
        let searchbar = UISearchBar()
+        searchbar.backgroundColor = UIColor.DarkGray1
         searchbar.placeholder = "검색어를 입력해주세요."
-        searchbar.searchTextField.layer.cornerRadius = 15
+        searchbar.searchTextField.backgroundColor = UIColor.DarkGray3
+        searchbar.searchTextField.layer.cornerRadius = 3
         searchbar.searchTextField.layer.masksToBounds = true
+        searchbar.searchTextField.textColor = .white
+        searchbar.setImage(UIImage(), for: .search, state: .normal)
         searchbar.setImage(UIImage(named: "icCancel"), for: .clear, state: .normal)
         return searchbar
     }()
@@ -29,20 +33,32 @@ class SearchViewController: UIViewController {
     lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.text = "최근 검색어"
-        label.textColor = .black
-        label.font = UIFont.boldSystemFont(ofSize: 17)
+        label.font = UIFont.caption11
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        return label
+    }()
+    
+    lazy var noSearchLabel: UILabel = {
+        let label = UILabel()
+        label.text = "검색 내역이 없습니다."
+        label.font = UIFont.caption11
+        label.textColor = UIColor.DarkGray3
+        label.font = UIFont.systemFont(ofSize: 17)
         return label
     }()
     
     lazy var alldeleteLabel: UIButton = {
         let button = UIButton()
-        button.setTitle("전체 삭제", for: .normal)
-        button.setTitleColor(.lightGray, for: .normal)
+        let attributedTitle = NSMutableAttributedString(string: "전체삭제",
+                              attributes: [NSAttributedString.Key.font : UIFont.caption11,
+                                           NSAttributedString.Key.foregroundColor: UIColor.white,
+                                           NSAttributedString.Key.underlineStyle: NSUnderlineStyle.single.rawValue])
+        button.setAttributedTitle(attributedTitle, for: .normal)
         button.titleLabel?.font = UIFont.systemFont(ofSize: 13)
         button.addTarget(self, action: #selector(deleteAllSearch(_:)), for: .touchUpInside)
         return button
     }()
-    
     
     lazy var bodytitlestackView: UIStackView = {
         let stackV = UIStackView(arrangedSubviews: [titleLabel, alldeleteLabel])
@@ -52,29 +68,44 @@ class SearchViewController: UIViewController {
         return stackV
     }()
     
-    lazy var searchTableView: UITableView = {
-        let cv = UITableView()
-        cv.register(SearchTableViewCell.self, forCellReuseIdentifier: cellId)
+    lazy var workLabel: UILabel = {
+        let label = UILabel()
+        label.text = "원하는 직무도\n함께 검색 해보세요!"
+        label.font = UIFont.caption11
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        return label
+    }()
+    
+    lazy var resultCell: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.minimumLineSpacing = .zero
+        flowLayout.minimumInteritemSpacing = 16
+        flowLayout.scrollDirection = .horizontal
+        flowLayout.sectionInset = .init(top: 5, left: 16, bottom: 5, right: 16)
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        cv.register(ResultCell.self, forCellWithReuseIdentifier: cellId)
+        cv.backgroundColor = UIColor.DarkGray1
+        cv.showsHorizontalScrollIndicator = false
         return cv
     }()
 
     // MARK: - Lift Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        searchTableView.delegate = self
-        searchTableView.dataSource = self
-        searchbar.delegate = self
-        setNav()
-        setUp()
-        self.searchbar.becomeFirstResponder()
+        configureNav()
+        configureUI()
+        setDelegate()
     }
     
     // MARK: - Methods
-    func setUp() {
+    func configureUI() {
+        view.backgroundColor = UIColor.DarkGray1
+        self.searchbar.becomeFirstResponder()
+
         view.addSubview(bodyview)
         bodyview.addSubview(bodytitlestackView)
-        bodyview.addSubview(searchTableView)
+        bodyview.addSubview(resultCell)
         
         bodyview.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(10)
@@ -83,55 +114,66 @@ class SearchViewController: UIViewController {
         
         bodytitlestackView.snp.makeConstraints { make in
             make.top.equalToSuperview()
-            make.centerX.equalToSuperview()
-            make.height.equalToSuperview().multipliedBy(0.08)
             make.width.equalToSuperview().multipliedBy(0.92)
+            make.height.equalToSuperview().multipliedBy(0.08)
+            make.centerX.equalToSuperview()
         }
-        
-        searchTableView.snp.makeConstraints { make in
-            make.top.equalTo(bodytitlestackView.snp.bottom)
-            make.leading.trailing.equalToSuperview()
-            make.bottom.equalToSuperview()
+        resultCell.snp.makeConstraints { make in
+            make.top.equalTo(bodytitlestackView.snp.bottom).offset(-10)
+            make.width.equalToSuperview().multipliedBy(0.94)
+            make.height.equalToSuperview().multipliedBy(0.1)
+            make.centerX.equalToSuperview()
+
         }
     }
 
-    func setNav() {
+    func configurenoSearchLabel() {
+        bodyview.addSubview(noSearchLabel)
+        noSearchLabel.snp.makeConstraints { make in
+            make.top.equalTo(bodytitlestackView.snp.bottom).offset(10)
+            make.leading.equalTo(titleLabel.snp.leading).offset(10)
+            make.centerX.equalToSuperview()
+        }
+    }
+    
+    func configureNav() {
         self.navigationItem.titleView = searchbar
-        self.navigationController?.navigationBar.tintColor = .black
+        self.navigationController?.navigationBar.tintColor = .white
         self.navigationController?.navigationBar.topItem?.title = ""
     }
     
-    @objc func deleteSearch(_ sender:UIButton) {
-        let indexRow = sender.tag
-        list.remove(at: indexRow)
-        searchTableView.deleteRows(at: [IndexPath(row: indexRow, section: 0)], with: .left)
-        searchTableView.reloadData()
-        if (list == []) {
-            bodyview.removeFromSuperview()
-        }
+    func setDelegate() {
+        resultCell.delegate = self
+        resultCell.dataSource = self
+        searchbar.delegate = self
     }
     
     @objc func deleteAllSearch(_: UIButton) {
         list.removeAll()
-        searchTableView.reloadData()
-        bodyview.removeFromSuperview()
+        resultCell.reloadData()
+        resultCell.removeFromSuperview()
+        configurenoSearchLabel()
     }
 }
 
     // MARK: - extensions
-extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return list.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! SearchTableViewCell
-        cell.currentsearchText.text = list[indexPath.row]
-        cell.cancelButton.tag = indexPath.row
-        cell.cancelButton.addTarget(self, action: #selector(deleteSearch(_:)), for: .touchUpInside)
-        return cell
-    }
+     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+         return list.count
+     }
+     
+     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! ResultCell
+         cell.configureLabel(name: list[indexPath.item])
+         cell.contentView.layer.cornerRadius = cell.contentView.frame.height / 2
+         cell.contentView.backgroundColor = UIColor.DarkGray5
+         return cell
+     }
+     
+     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+         return ResultCell.fittingSize(availableHeight: 40, name: list[indexPath.item])
+     }
 }
 
 extension SearchViewController: UISearchBarDelegate {
@@ -140,10 +182,12 @@ extension SearchViewController: UISearchBarDelegate {
             return
         }
         if (list.isEmpty) {
-            setUp()
+            noSearchLabel.removeFromSuperview()
+            configureUI()
         }
         list.append(text)
         searchbar.text = ""
-        searchTableView.reloadData()
+        resultCell.reloadData()
     }
 }
+
