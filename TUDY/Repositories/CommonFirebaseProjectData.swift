f58d8a7b47e55895684019d766dfa611f4fc7aed
change-challenge <59588256+change-challenge@users.noreply.github.com>
2022-06-13 18:46:10 +0900

    [Fix] Network Error typo (#69)
    * [Feat] Add Firebase save func & model
    
    * [Fix]Conflict
    
    * [Feat] New 검색창 최근 검색어 레이아웃-(#55)
    - issue: #55
    - 최근 검색창을 collectionview로 구성
    - 하나씩 선택해서 삭제할 필요가 있는 지 의문이 들어 리스트만 보여주기로 만들 었습니다.
    
    * [Feat] 하단 WorkCollectionView 레이아웃 -#55
    
    * [Feat] 하단 CollectionView 레이아웃 -(#55)
    
    * [Feat] 하단 workCollectionView 수정 -(#55)
    
    * [Feat] Add SearchView Multiple Selected - #55
    
    * [Fix] Conflict & error
    
    * [Fix] ErrorMessage 수정 
    
    # [Type] Message 
    # Title은 최대 50글자 정도로만 입력
    # Title 끝에 마침표 금지, 무엇을 했는지 명확하게 작성
    ### ex)
    ### [Feat] message
    
    # --- Body ---
    # - issue: (#tagNumber)
    
    # --- Type ---
    #   Feat    : 새로운 기능 추가
    #   Fix     : 버그 수정
    #   Style   : 코드 의미에 영향을 주지 않는 변경사항 (세미콜론, 줄 간격 등등)
    #   Design  : Storyboard, UI 수정
    #   Refactor: 코드 리팩토링
    #   Test    : 모든 테스트 작업
    #   Docs    : 문서 추가 및 수정
    #   Chore   : 빌드 부분, 패키지 매니저, 세팅 관련 파일 추가 및 수정
    #   Rename  : 파일 네이밍 변경
    #   Remove  : 파일 제거
    #   !HOTFIX  : 빠른 버그수정 필요(해당 내용이 오류일 경우 앱 크래시 증상 등등)
    
    * # --- Head ---
    # [Type] Message 
    # Title은 최대 50글자 정도로만 입력
    # Title 끝에 마침표 금지, 무엇을 했는지 명확하게 작성
    ### ex)
    ### [Feat] message
    
    # --- Body ---
    # - issue: (#tagNumber)
    
    # --- Type ---
    #   Feat    : 새로운 기능 추가
    #   Fix     : 버그 수정
    #   Style   : 코드 의미에 영향을 주지 않는 변경사항 (세미콜론, 줄 간격 등등)
    #   Design  : Storyboard, UI 수정
    #   Refactor: 코드 리팩토링
    #   Test    : 모든 테스트 작업
    #   Docs    : 문서 추가 및 수정
    #   Chore   : 빌드 부분, 패키지 매니저, 세팅 관련 파일 추가 및 수정
    #   Rename  : 파일 네이밍 변경
    #   Remove  : 파일 제거
    #   !HOTFIX  : 빠른 버그수정 필요(해당 내용이 오류일 경우 앱 크래시 증상 등등)

---
+++ TUDY/Repositories/CommonFirebaseProjectData.swift
@@ -0,0 +1,50 @@
+//
+//  CommonFirebaseProjectData.swift
+//  TUDY
+//
+//  Created by Hojin Jang on 2022/06/13.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestore
+
+struct CommonFirebaseProjectData {
+    
+    private var documentListener: ListenerRegistration?
+    
+    static func saveProjectData(_ project: Project, completion: ((Error?) -> Void)? = nil) {
+        
+        var project = project
+        let db = Firestore.firestore()
+        let projectRef = db.collection("PROJECT").document()
+        
+        project.writeDate = Date().description
+        
+        guard let dictionary = project.asDictionary else {
+            print("[DEBUg] 프로젝트 Firebase 디버그 오류")
+            return
+        }
+        projectRef.setData(dictionary) { error in
+            if let error = error {
+                print("파이어베이스 저장 에러 \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    static func fetchProject(completion: @escaping([Project]) -> Void) {
+        var projects: [Project] = []
+        Firestore.firestore().collection("PROJECT").getDocuments { snapshot, error in
+            if let error = error {
+                print("DEBUG: 프로젝트 정보 가져오기 실패 \(error.localizedDescription)")
+                return
+            }
+            snapshot?.documents.forEach({ document in
+                let dict = document.data()
+                let project = Project(dict: dict)
+                projects.append(project)
+            })
+            completion(projects)
+        }
+    }
+}
