e9eb108f5bbfd921582dde68393474dd31ce6ac2
neuli <asdfz888@naver.com>
2022-06-15 15:14:43 +0900

    [Fix] messageID 삭제

--- TUDY/Models/Message.swift
+++ TUDY/Models/Message.swift
@@ -8,32 +8,19 @@
 import Foundation
 
 struct Message: Codable {
-    let messageID: String
     let content: String
     let sender: User
     let createdDate: String
     
-    init(messageID: String,
-         content: String,
-         sender: User,
-         createdDate: String) {
-        self.messageID = messageID
-        self.content = content
-        self.sender = sender
-        self.createdDate = createdDate
-    }
-    
     init(content: String,
          sender: User,
          createdDate: String) {
-        self.messageID = UUID().uuidString
         self.content = content
         self.sender = sender
         self.createdDate = createdDate
     }
     
     init(dict: [String : Any]) {
-        self.messageID = dict["messageID"] as? String ?? ""
         self.content = dict["content"] as? String ?? ""
         self.sender = dict["sender"] as? User ?? User()
         self.createdDate = dict["createdDate"] as? String ?? ""
