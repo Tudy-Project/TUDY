5fdec4c643f62ea0a0a7511b5b0db91289eecff5
change-challenge <dog941128@hotmail.com>
2022-06-15 18:01:14 +0900

    [Feat] 개인채팅방 bottomsheet 중 tableView 작업 중

--- TUDY/Presentations/Chat/ViewController/InvitedViewController.swift
+++ TUDY/Presentations/Chat/ViewController/InvitedViewController.swift
@@ -9,33 +9,67 @@ import UIKit
 
 class InvitedViewController: UIViewController {
     // MARK: - Properties
+    
+    private lazy var groupChatList = [
+        ChatInfo(chatState: .groupChat,
+                 chatTitle: "그룹챗 테스트 그룹챗 테스트 그룹챗 테스트 그룹챗 테스트",
+                 participantIDs: ["", "", ""]),
+        ChatInfo(chatState: .groupChat,
+                 chatTitle: "그룹챗 테스트2",
+                 participantIDs: ["", ""])
+    ]
+    
     private lazy var dimmedView: UIView = {
         let view = UIView()
-        view.backgroundColor = UIColor.darkGray.withAlphaComponent(0.7)
+        view.backgroundColor = .DarkGray1
         return view
     }()
     
     private lazy var bottomSheetView: UIView = {
         let view = UIView()
-        view.backgroundColor = .white
-        view.layer.cornerRadius = 10
+        view.backgroundColor = .DarkGray3
+        view.layer.cornerRadius = 19
         view.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
         view.clipsToBounds = true
         return view
+    }()
+    
+    private var bottomsheetStackView: UIStackView = {
+        let stackview = UIStackView()
+        stackview.isLayoutMarginsRelativeArrangement = true
+        stackview.axis = .horizontal
+        stackview.layoutMargins.left = 15.0
+        stackview.layoutMargins.right = 15.0
+        return stackview
+    }()
+    
+    private lazy var bottomsheetgroupchatListLabel: UILabel = {
+        let label = UILabel().label(text: "스터디챗 목록", font: .sub20)
+        return label
+    }()
+    
+    private lazy var bottomsheetgroupchatinvitedbutton: UILabel = {
+        let label = UILabel().label(text: "초대", font: .sub16)
+        let tap = UITapGestureRecognizer(target: self, action: #selector(bottomsheetgroupchatinvitedbuttonClicked))
+        label.isUserInteractionEnabled = true
+        label.addGestureRecognizer(tap)
+        label.textColor = UIColor.PointBlue
+        return label
     }()
 
     private var bottomSheetViewTopConstraint: NSLayoutConstraint!
     
     private var defaultHeight: CGFloat = 300
+    
+    private var groupChatListTableView: UITableView!
+
 
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configureUI()
-        
-        let dimmedTap = UITapGestureRecognizer(target: self, action: #selector(dimmedViewTapped(_:)))
-        dimmedView.addGestureRecognizer(dimmedTap)
-        dimmedView.isUserInteractionEnabled = true
+        configurebottomSheetUI()
+        configureTableView()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -44,25 +78,59 @@ class InvitedViewController: UIViewController {
     }
 }
 
+// MARK: - extensions about setting UI
 extension InvitedViewController {
-    func configureUI() {
+    private func configureUI() {
         view.addSubview(dimmedView)
         view.addSubview(bottomSheetView)
         
         dimmedView.alpha = 0.0
-
+        let dimmedTap = UITapGestureRecognizer(target: self, action: #selector(dimmedViewTapped(_:)))
+        dimmedView.addGestureRecognizer(dimmedTap)
+        dimmedView.isUserInteractionEnabled = true
+        
         dimmedView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
         
         bottomSheetView.snp.makeConstraints { make in
-            make.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.bottom)
             make.bottom.equalToSuperview()
-            make.top.equalTo(900)
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide)
         }
     }
+
+    private func configurebottomSheetUI() {
+        bottomSheetView.addSubview(bottomsheetStackView)
+        
+        bottomsheetStackView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(15)
+            make.width.equalToSuperview().multipliedBy(0.9)
+            make.centerX.equalToSuperview()
+        }
     
-    func showBottomSheet() {
+        bottomsheetStackView.addArrangedSubview(bottomsheetgroupchatListLabel)
+        bottomsheetStackView.addArrangedSubview(bottomsheetgroupchatinvitedbutton)
+        bottomsheetStackView.setCustomSpacing(130, after: bottomsheetgroupchatListLabel)
+    }
+    
+    private func configureTableView() {
+        groupChatListTableView = UITableView(frame: view.bounds, style: .plain)
+//        groupChatListTableView.delegate = self
+        groupChatListTableView.register(PersonalBottomSheetTableViewCell.self, forCellReuseIdentifier: PersonalBottomSheetTableViewCell.reuseIdentifier)
+        groupChatListTableView.rowHeight = 108
+        groupChatListTableView.separatorColor = .LightGray2
+        groupChatListTableView.separatorInset = .init(top: 0, left: 13, bottom: 0, right: 13)
+
+    }
+    
+    @objc func bottomsheetgroupchatinvitedbuttonClicked(sender:UITapGestureRecognizer) {
+        print("tap working")
+    }
+}
+
+extension InvitedViewController {
+    private func showBottomSheet() {
         bottomSheetView.snp.remakeConstraints { make in
             make.top.equalTo(500)
             make.bottom.equalToSuperview()
@@ -71,14 +139,14 @@ extension InvitedViewController {
         }
 
         UIView.animate(withDuration: 0.25, delay: 0, options: .curveEaseIn, animations: {
-            self.dimmedView.alpha = 0.7
+            self.dimmedView.alpha = 0.8
             self.view.layoutIfNeeded()
         }, completion: nil)
     }
     
     private func hideBottomSheetAndGoBack() {
         bottomSheetView.snp.remakeConstraints { make in
-            make.top.equalTo(900)
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.bottom)
             make.bottom.equalToSuperview()
             make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading)
             make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing)
