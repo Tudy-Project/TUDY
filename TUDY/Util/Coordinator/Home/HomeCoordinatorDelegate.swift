9ea20015c9108bec1dae84b2a4a49a01692eb147
neuli <asdfz888@naver.com>
2022-06-16 18:14:04 +0900

    [Feat] 코디네이터 변경 채팅보내기 버튼 (미완)

---
+++ TUDY/Util/Coordinator/Home/HomeCoordinatorDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  HomeCoordinatorDelegate.swift
+//  TUDY
+//
+//  Created by neuli on 2022/06/16.
+//
+
+import Foundation
+
+protocol HomeCoordinatorDelegate: AnyObject {
+    func showPersonalChat(with projectWriter : String)
+}
