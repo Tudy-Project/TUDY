87e1dfd00eaf6d4acd8b14849e05d484a0bec81d
하늘이 <39167842+NEULiee@users.noreply.github.com>
2022-07-12 21:20:34 +0900

    토큰 가져오기 (#127)
    * [Feat] 토큰 가져오기
    
    * [Fix] 에러 메세지 수정

--- TUDY/Repositories/FirebaseFCMToken.swift
+++ TUDY/Repositories/FirebaseFCMToken.swift
@@ -19,18 +19,35 @@ struct FirebaseFCMToken {
         let userID = FirebaseUser.getUserID()
         let fcmToken = FCMToken(userID: userID, fcmToken: token)
         
-        let collectionListener = tokenPath.document(userID)
+        let path = tokenPath.document(userID)
         
         guard let dictionary = fcmToken.asDictionary else {
             print("[DEBUG] 토큰 데이터 로딩 실패")
             return
         }
         
-        collectionListener.setData(dictionary) { error in
+        path.setData(dictionary) { error in
             if let error = error {
                 print("파이어베이스 저장 에러 \(error.localizedDescription)")
                 return
             }
         }
+    }
+    
+    static func fetchFCMToken(userID: String, completion: @escaping (String) -> Void) {
+        
+        tokenPath
+            .whereField("userID", isEqualTo: userID)
+            .getDocuments { snapshot, error in
+                if let error = error {
+                    print("[DEBUG] 토큰 가져오기 실패 \(error.localizedDescription)")
+                    return
+                }
+                snapshot?.documents.forEach({ document in
+                    let dict = document.data()
+                    let fcmToken = FCMToken(dict: dict)
+                    completion(fcmToken.fcmToken)
+                })
+            }
     }
 }
